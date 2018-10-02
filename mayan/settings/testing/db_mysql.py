3b6c3fd2b15b9d3217482bfdfecb7d8c0e6766d5
leonardo <leonardo@debian64>
2018-10-02 11:52:28 +0200

    Configurations and dockerfiles

--- mayan/settings/testing/db_mysql.py
+++ mayan/settings/testing/db_mysql.py
@@ -1,13 +1,18 @@
 from __future__ import unicode_literals
+import os
 
 from .base import *  # NOQA
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'mayan_edms',
-        'USER': 'root',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'NAME': os.getenv('DATABASE_NAME', 'mayan_edms'),
+        'USER': os.getenv('DATABASE_USER', 'root'),
+        'HOST': os.getenv('DATABASE_HOST', '127.0.0.1'),
+        'PASS': os.getenv('DATABASE_PASS', ''),
+        'PORT': os.getenv('DATABASE_PORT', '3306'),
     }
 }
+
+BROKER_URL = os.getenv('REDIS_URL', 'redis://127.0.0.1:6379/0')
+CELERY_RESULT_BACKEND = os.getenv('REDIS_URL', 'redis://127.0.0.1:6379/0')
